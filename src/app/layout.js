5916bd21d02cebff49282a2ec2bc50cf67b332c3
Rene Baine <renebaine04@gmail.com>
2026-05-08 19:46:41 -0400

    Added changes to Navbar size

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Edu_AU_VIC_WA_NT_Pre  } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "../components/navbar";
 
 
+const eduFont = Edu_AU_VIC_WA_NT_Pre({
+  subsets: ["latin"],
+  variable: "--font-edu",
+})
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,9 +26,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${eduFont.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-amber-200">
         <Navbar/>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col flex-1 items-center justify-center bg-amber-900 font-sans">
-        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 sm:items-center">
+      <div className="flex flex-col items-center justify-center bg-amber-200 text-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center pb-32 pt-20 px-16 sm:items-center">
           <div className="flex flex-col items-center gap-2 text-center sm:items-center">
-            <h1 className="max-w-prose  text-7xl lg:text-8xl md:text-7xl sm:text-6xl font-black tracking-tight text-amber-50 dark:zinc-50">
-              Recipe<span className="font-serif italic font-normal"> Bank</span>
+            <h1 className="max-w-prose text-8xl lg:text-9xl md:text-7xl sm:text-6xl font-bold font-serif tracking-tight text-black dark:zinc-50">
+              Recipe
+              <br />
+              <span className="font-edu font-normal"> Bank</span>
             </h1>
-            <p className="max-w-md text-lg leading-7 font-serif italic text-amber-50">
+            <p className="max-w-md text-lg leading-7 font-serif italic">
               Share your recipe and inspire others today. Upload all you recipe
               needs and learn from others.
             </p>
@@ -21,7 +23,7 @@ export default function Home() {
               href=""
               rel="noopener noreferrer"
             >
-              Sign In
+              Join
             </a>
             <a
               className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black px-5 transition-colors hover:border-transparent hover:bg-amber-600 dark:border-white/[.145] dark:border-4 dark:hover:bg-amber-600 md:w-[158px]"
@@ -32,6 +34,12 @@ export default function Home() {
             </a>
           </div>
         </main>
+        <div className="flex flex-row bg-gray-600 py-24 w-full items-center gap-2 text-center sm:items-center">
+          <div className="w-1/2">
+          <h2 classname=" text-8xl lg:text-9xl md:text-7xl sm:text-6xl font-bold font-serif tracking-tight text-amber-200 dark:zinc-50">This is the content for the first page</h2>
+          </div>
+          <div classname="w-1/2 bg-blue-700"> this is the content for the second page </div>
+        </div>
       </div>
     </>
   );
